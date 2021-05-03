24e6de2808d01937e1157d8f2c22a810fbc049b8
mohit <mohit.tiwari_cs18@gla.ac.in>
2021-05-03 07:56:37 +0530

    commit

---
+++ views/common/nav.js
@@ -0,0 +1,9 @@
+
+<ol >
+    <li style="display:inline"><a href="/profile/mohit">Profile PAge</a></li>
+    <li style="display:inline"><a href="/home">Home PAge</a></li>
+    <li style="display:inline"><a href="/login">Login PAge</a></li>
+</ol>
+
+<link rel="stylesheet" href="../assets/style.css"></link>
+
