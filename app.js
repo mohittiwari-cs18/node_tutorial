2eee3cd640b55395f8ef717f774060774c5ae43c
mohit <mohit.tiwari_cs18@gla.ac.in>
2021-05-04 07:32:48 +0530

    commit

--- app.js
+++ app.js
@@ -34,18 +34,26 @@
 // const express =require('express')
 // const app=express();
 // app.use('/assets',express.static('assets'))
+// var bodyParser=require('body-parser')
+// var encoder=bodyParser.urlencoded();
 // app.set('view engine','ejs')
 // app.get ('/profile/:name',function(req,res){
 //     data={email:"mohit@gmail.com",gender:"male",skills:["java","php","nodejs"]}
 //    res.render('profile',{name:req.params.name,data:data})
 // })
+// app.post("/login",encoder,function(req,res){
+//    console.log(req.body)
+//     res.render('home')
+//  })
 // app.get ('/home',function(req,res){
 //    res.render('home')
 // })
+
 // app.get ('/login',function(req,res){
-//    console.log(req.query)
-//     res.render('login')
-//  })
+   
+//    res.render('login')
+// })
+
 
 // app.listen(4500)
 
@@ -56,14 +64,45 @@ const express= require('express')
 const app=express();
 const mongoose=require('mongoose')
 const User=require('./models/users')
-
+var bodyParser=require('body-parser')
+var jsonParser=bodyParser.json();
 mongoose.connect('mongodb+srv://mohit:xcBkk5YAmpzohyHX@cluster0.hxzo4.mongodb.net/node?retryWrites=true&w=majority',
 {
    useNewUrlParser:true,
    useUnifiedTopology:true
 });
 
-User.find({},function(err,users){
-if(err) console.log(err);
-console.log(users)
+app.get('/users',function(req,res){
+   User.find()
+   .then((data)=>{
+      res.json(data)
+   })
 })
+
+app.post('/user',jsonParser,function(req,res){
+   const data=new User({
+      _id:new mongoose.Types.ObjectId,
+      name:req.body.name,
+      email:req.body.email,
+      address:req.body.address
+   })
+   data.save().then((result)=>{
+      res.status(201).json(result)
+   })
+   .catch((error)=>{
+      console.log(error)
+   })
+ 
+})
+app.delete('/user/:id',function(req,res){
+   User.deleteOne({_id:req.params.id})
+   .then((result)=>{
+      res.status(200).json(result)
+   })
+   .catch((err)=>{
+      console.log(err)
+   })
+})
+
+
+app.listen(4500)
